d303559645c1ffbbbb1b52a81663bcf3d2654829
Mike Herwig <mike.herwig@external.t-systems.com>
2020-05-11 13:55:57 +0200

    changed metrics endpoint to only accept requests from 127.0.0.1

--- extensions/keycloak-metrics-spi/src/main/java/org/jboss/aerogear/keycloak/metrics/MetricsEndpoint.java
+++ extensions/keycloak-metrics-spi/src/main/java/org/jboss/aerogear/keycloak/metrics/MetricsEndpoint.java
@@ -1,18 +1,22 @@
 package org.jboss.aerogear.keycloak.metrics;
 
-import org.keycloak.services.resource.RealmResourceProvider;
-
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.StreamingOutput;
+
+import org.keycloak.services.resource.RealmResourceProvider;
 
 public class MetricsEndpoint implements RealmResourceProvider {
 
     // The ID of the provider is also used as the name of the endpoint
     public final static String ID = "metrics";
-
+    
     @Override
     public Object getResource() {
         return this;
@@ -20,7 +24,15 @@ public class MetricsEndpoint implements RealmResourceProvider {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public Response get() {
+    public Response get(@Context HttpServletRequest request) {
+    	String remoteAddress = request.getRemoteAddr();
+    	
+    	if (!("127.0.0.1".equals(remoteAddress) || "0:0:0:0:0:0:0:1".equals(remoteAddress))) {
+    		final String forbiddenMessage = "Calling the metrics endpoint from outside is not allowed";
+    		
+    		throw new WebApplicationException(Response.status(Status.FORBIDDEN).entity(forbiddenMessage).build());
+    	}
+    	
         final StreamingOutput stream = output -> PrometheusExporter.instance().export(output);
         return Response.ok(stream).build();
     }
