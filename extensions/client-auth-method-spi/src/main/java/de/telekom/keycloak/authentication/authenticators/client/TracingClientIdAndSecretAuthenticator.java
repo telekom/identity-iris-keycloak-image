3889b65a634cfb6aec46f9562befd15b2a48bd76
konstantin_evo <konstantin.priluchnyi@telekom.com>
2025-04-17 11:57:09 +0300

    docs: add licensing and copyright information

--- extensions/client-auth-method-spi/src/main/java/de/telekom/keycloak/authentication/authenticators/client/TracingClientIdAndSecretAuthenticator.java
+++ extensions/client-auth-method-spi/src/main/java/de/telekom/keycloak/authentication/authenticators/client/TracingClientIdAndSecretAuthenticator.java
@@ -1,3 +1,7 @@
+//SPDX-FileCopyrightText: 2023 Deutsche Telekom AG
+//
+//SPDX-License-Identifier: Apache-2.0
+
 package de.telekom.keycloak.authentication.authenticators.client;
 
 import io.prometheus.client.Counter;
