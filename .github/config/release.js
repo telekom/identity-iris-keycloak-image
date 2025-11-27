f335e0a0cffe3197b0913f7b2ce6590b8728d988
andrewRdk <andrei.rudkov@telekom.com>
2025-11-27 10:48:46 +0200

    chore: yaml to js

---
+++ .github/config/release.js
@@ -0,0 +1,16 @@
+//  SPDX-FileCopyrightText: 2025 Deutsche Telekom AG
+//
+//  SPDX-License-Identifier: Apache-2.0
+
+export default {
+    branches: ['main', 'action-release'],
+    tagFormat: '${version}',
+    repositoryUrl: 'git@github.com:telekom/identity-iris-keycloak-image.git',
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        'semantic-release-export-data',
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/changelog',
+        '@semantic-release/github'
+    ]
+};
