64acfba1fb0fb17402ab33084ce8c345f80fe14e
andrewRdk <andrei.rudkov@telekom.com>
2025-11-27 13:34:45 +0200

    chore: skip actions on automated changelog changes

--- .github/config/release.js
+++ .github/config/release.js
@@ -20,7 +20,7 @@ export default {
             '@semantic-release/git',
             {
                 "assets": ["CHANGELOG.md"],
-                "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+                "message": "chore(release): ${nextRelease.version} [skip actions]\n\n${nextRelease.notes}"
             }
         ],
         '@semantic-release/github'
